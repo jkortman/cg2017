3e0771bb67bcac81e136a573fc0e37fe2468a8e2
jkortman <james.kortman@gmail.com>
2017-05-19 12:19:52 +0930

    Added orientation data to object

--- src/Object.hpp
+++ src/Object.hpp
@@ -4,10 +4,18 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
-class Object
+#include "Entity.hpp"
+#include "Core.hpp"
+
+#include <glm/glm.hpp>
+
+class Object: public Entity
 {
 public:
     Mesh* mesh;
+    glm::vec3 position
+    glm::mat4 model_matrix;
+    ShaderID shader_program;
 };
 
 #endif
