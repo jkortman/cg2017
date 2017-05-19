cc9067c6b477e81570731ab773c8544cb0e43852
jkortman <james.kortman@gmail.com>
2017-05-19 12:14:18 +0930

    Added file structure to src dir

---
+++ src/Object.hpp
@@ -0,0 +1,13 @@
+// Object class
+// A physical object in the world with some shape, position, orientation.
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+class Object
+{
+public:
+    Mesh* mesh;
+};
+
+#endif
