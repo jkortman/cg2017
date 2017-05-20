e1955bcab98eb8e09933e78205df343d7063a3cd
= <=>
2017-05-20 09:33:38 +0930

    Console read() func partially implemented: leading/trailing spaces removed, input tokenised. main.cpp modified - moved InputHandler::initialize() to after renderer.initialise() (so glfwCreateWindow is called before glfwGetCurosorPos is)

---
+++ src/Console.cpp
@@ -0,0 +1,50 @@
+// Implementation of Console class member functions.
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Console.hpp"
+
+std::vector<std::string> tokenize(
+    std::string str,
+    const std::string delims)
+{
+    std::vector<std::string> tokens;
+    int pos = str.find(delims);
+
+    while (pos != -1)
+    {
+        tokens.push_back(str.substr(0, pos));
+        str.erase(0,pos+1);
+        pos = str.find(delims);
+    }
+    tokens.push_back(str);
+    return tokens;
+}
+
+int Console::parse()
+{
+    // Load input
+    std::string input;
+    getline(std::cin, input);
+
+    if ( input.size() == 0) return -1; // No input
+
+    // Strip leading/trailing spaces
+    int start = input.find_first_not_of(" ");
+    int end = input.find_last_not_of(" ");
+    if (start == -1) start = 0; // No leading spaces
+    if (end == -1) end = 0;     // No trailing spaces
+    input = input.substr(start,end-start+1);
+
+    if ( input.size() == 1) return -1; // Only spaces
+    
+    // Tokenise input
+    std::vector<std::string> InputStorage = tokenize(input, " ");
+
+    // Handle input
+    return 0;
+}
+
+
---
+++ src/Console.hpp
@@ -0,0 +1,17 @@
+// Console class
+// A class to manage user input via the terminal.
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+class Console
+{
+public:
+	int parse();
+    // get(var);
+    // set(var,value);
+    // register(type,var,ptr, writeable=true, what='');
+
+};
+
+#endif // CONSOLE_H
