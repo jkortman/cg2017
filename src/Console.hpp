013993610a404b15e9a716e0c417f957f6273837
jthughes@users.noreply.github.com <jthughes@users.no-reply.github.com>
2017-05-20 23:33:27 +0930

    console has basic interactivity, can register variables

--- src/Console.hpp
+++ src/Console.hpp
@@ -4,14 +4,16 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
+enum VarType {Invalid, Function, Int, Float, Bool};
+
 class Console
 {
 public:
+	void initialize();
 	int parse();
     // get(std::string var);
     // set(std::string var, std::string value);
-    // register(type,var,ptr, writeable=true, what='');
-
+    void register_var(VarType type, std::string var, void* ptr, bool writeable=true, std::string what="");
 };
 
 #endif // CONSOLE_H
