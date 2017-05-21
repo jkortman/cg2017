69c56f06e6887654298986f113673df3b65f8da1
jkortman <james.kortman@gmail.com>
2017-05-21 20:39:45 +0930

    Added working movement and mouselook

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -6,6 +6,7 @@
 #include <GL/glew.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <glm/gtx/rotate_vector.hpp>
 #include <stdexcept>
 
 Scene::Scene()
@@ -13,16 +14,57 @@ Scene::Scene()
 
 void Scene::update(float dt)
 {
+    const float rotate_factor = 0.006f;
+    const float move_speed = 0.13f;
+
+    glm::vec3 right_direction = glm::cross(player.direction, AXIS_Y);
+
     if (InputHandler::keys[GLFW_KEY_W])
     {
-        player.position.z += 0.2;
+        player.position.x += move_speed * player.direction.x;
+        //player.position.y += move_speed * player.direction.y;
+        player.position.z += move_speed * player.direction.z;
     }
     if (InputHandler::keys[GLFW_KEY_S])
     {
-        player.position.z -= 0.2;
+        player.position.x -= move_speed * player.direction.x;
+        //player.position.y -= move_speed * player.direction.y;
+        player.position.z -= move_speed * player.direction.z;
+    }
+    if (InputHandler::keys[GLFW_KEY_A])
+    {
+        player.position.x -= move_speed * right_direction.x;
+        //player.position.y -= move_speed * right_direction.y;
+        player.position.z -= move_speed * right_direction.z;
+    }
+    if (InputHandler::keys[GLFW_KEY_D])
+    {
+        player.position.x += move_speed * right_direction.x;
+        //player.position.y += move_speed * right_direction.y;
+        player.position.z += move_speed * right_direction.z;
+    }
+    if (InputHandler::keys[GLFW_KEY_SPACE])
+    {
+        player.position.y += move_speed;
+    }
+    if (InputHandler::keys[GLFW_KEY_LEFT_SHIFT])
+    {
+        player.position.y -= move_speed;
     }
 
+    // Rotations
+    player.direction = glm::rotate(
+        player.direction,
+        rotate_factor * float(-InputHandler::mouse_dx),
+        AXIS_Y);
+    player.direction = glm::rotate(
+        player.direction,
+        rotate_factor * float(-InputHandler::mouse_dy),
+        right_direction);
+    //player.direction = glm::normalize(player.direction);
+
     camera.position = player.position;
+    camera.direction = player.direction;
     camera.update_view();
 
     // Update the model and normal matrices for each object.
